a93acfd23f5cb54031b739473235fa1793c54ae4
Adielson <adielsonzx05@gmail.com>
2024-12-09 15:26:18 -0300

    beta

---
+++ enumarador_dns.php
@@ -0,0 +1,29 @@
+<?php
+function enumerarDNS($dominio) {
+
+    $tipos = ['A', 'MX', 'NS', 'TXT', 'SOA'];
+    
+    foreach ($tipos as $tipo) {
+        echo "Consultando registros $tipo para $dominio...\n";
+
+        $registros = dns_get_record($dominio, constant("DNS_$tipo"));
+        
+        if (!empty($registros)) {
+            foreach ($registros as $registro) {
+                print_r($registro);
+            }
+        } else {
+            echo "Nenhum registro $tipo encontrado.\n";
+        }
+        echo "\n";
+    }
+}
+
+if (isset($argv[1])) {
+    $dominio = $argv[1];
+    enumerarDNS($dominio);
+} else {
+    echo "Por favor, forneça um domínio como argumento.\n";
+    echo "Exemplo de uso: php dns_enumerador.php exemplo.com\n";
+}
+?>
